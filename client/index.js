c305a9df41c87ecbccf4dc0800f3ca38f6f5f4f2
Dean Shub <deanshub@gmail.com>
2016-12-11 08:24:06 +0200

    adding title component

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
-
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import React from 'react'
+import DocumentTitle from 'react-document-title'
 
 import App from './containers/App'
 import configure from './store'
@@ -12,11 +12,14 @@ const store = configure()
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-      </Route>
-    </Router>
-  </Provider>,
+  <DocumentTitle title="Hexxagon">
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </Provider>
+  </DocumentTitle>
+  ,
   document.getElementById('root')
 )
